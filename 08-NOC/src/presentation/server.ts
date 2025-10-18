2f191d68f919b089d7935a06d5523c1f0fc5a84c
juanib99 <juanignaciobisello@hotmail.com>
2025-10-18 13:43:37 -0300

    nodemailer en funcionamiento

--- 08-NOC/src/presentation/server.ts
+++ 08-NOC/src/presentation/server.ts
@@ -2,6 +2,7 @@ import { checkService } from "../domain/use-cases/checks/check-service";
 import { fileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository";
 import { CronService } from "./cron/cron-service";
+import { EmailService } from "./email/email.service";
 
 
 const fileSystemLogRepository = new LogRepositoryImpl(
@@ -14,18 +15,28 @@ export class Server {
     public static start() {
         console.log('Server started...');
 
-        CronService.createJob( 
-            '*/5 * * * * *', 
-            () => {
-                const url = 'https://www.google.com';
-                // const url = 'http://localhost:3000/posts';
-                new checkService(
-                    fileSystemLogRepository,
-                    undefined,
-                    undefined
-                ).execute( url);
-                // new checkService().execute( 'http://localhost:3000/posts');
-            }
-        );
+        const emailService = new EmailService();
+
+        emailService.sendEmail({
+            to: 'ignaciodxtree99@live.com.ar',
+            subject: 'Test Email from Node.js NOC',
+            htmlBody: `
+                <h1>This is a test email sent from Node.js using Nodemailer!</h1>
+                <p>Hello! This email confirms that the email service is working correctly.</p>
+                `
+        })
+        // CronService.createJob( 
+        //     '*/5 * * * * *', 
+        //     () => {
+        //         const url = 'https://www.google.com';
+        //         // const url = 'http://localhost:3000/posts';
+        //         new checkService(
+        //             fileSystemLogRepository,
+        //             undefined,
+        //             undefined
+        //         ).execute( url);
+        //         // new checkService().execute( 'http://localhost:3000/posts');
+        //     }
+        // );
     }
 }
