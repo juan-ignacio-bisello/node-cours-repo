9c37e3deefa2f95365dad0544826944f0bbce038
juanib99 <juanignaciobisello@hotmail.com>
2025-10-20 16:45:46 -0300

    prueba de mongoDB

--- 08-NOC/src/config/plugins/envs.config.ts
+++ 08-NOC/src/config/plugins/envs.config.ts
@@ -7,5 +7,11 @@ export const envs = {
     MAILER_SERVICE: env.get('MAILER_SERVICE').required().asString(),
     MAILER_EMAIL: env.get('MAILER_EMAIL').required().asEmailString(),
     MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString(),
-    PROD: env.get('PROD').required().asBool()
+    PROD: env.get('PROD').required().asBool(),
+
+    //mongo
+    MONGO_URL: env.get('MONGO_URL').required().asString(),
+    MONGO_DB_NAME: env.get('MONGO_DB_NAME').required().asString(),
+    MONGO_USER: env.get('MONGO_USER').required().asString(),
+    MONGO_PASSWORD: env.get('MONGO_PASSWORD').required().asString(),
 }
